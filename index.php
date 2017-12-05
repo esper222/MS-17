e6da01a0318fb9168e227c99ad9ec9b139951deb
esper222 <32288780+esper222@users.noreply.github.com>
2017-12-05 10:45:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ echo "Hello world"
 
 echo "<strong>Mina olen teine lause</strong>"
 
-?>
+
 $hello = 'Tere';
 $test = array (5,3,33,14);
 if (is_array($test)){
@@ -16,3 +16,4 @@ if (is_array($test)){
  for ($1=0; $i < count($test); $i++) {
  echo $test[$i]."<br>";
  }
+?>
