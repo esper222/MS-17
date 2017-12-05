eab2231be6c4ad00bf4f1fc2554cde6a375459ad
esper222 <32288780+esper222@users.noreply.github.com>
2017-12-05 10:44:48 +0200

    index.php
    05.12.2017

--- index.php
+++ index.php
@@ -3,4 +3,16 @@
 echo "Hello world"
 
 echo "<strong>Mina olen teine lause</strong>"
+
 ?>
+$hello = 'Tere';
+$test = array (5,3,33,14);
+if (is_array($test)){
+  forceach($test as $value){
+  echo $value."<br>";
+  }
+ } else {echo "ei ole massiiv";}
+ 
+ for ($1=0; $i < count($test); $i++) {
+ echo $test[$i]."<br>";
+ }
